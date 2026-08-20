2561a5a633432dc2009f22d18ea814fd15b327bc
Danish Shamshee <danishshamshee123@gmail.com>
2026-08-21 01:05:16 +0530

    fix some typo

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <div className="z-20 w-full border-t border-border/50 bg-background/80 backdrop-blur-xl supports-[backdrop-filter]:bg-background/60 print:hidden">
       <div className="mx-4 flex h-14 items-center justify-between sm:mx-8">
         <p className="text-xs font-medium text-muted-foreground md:text-sm">
-          &copy; {new Date().getFullYear()} Magnetix Infosystems Devlopment Pvt Ltd. All rights reserved.
+          &copy; {new Date().getFullYear()} Magnetix Infosystems Development Pvt Ltd. All rights reserved.
         </p>
         <p className="text-xs font-medium text-muted-foreground md:text-sm">
           Developed by Danish Shamshee
